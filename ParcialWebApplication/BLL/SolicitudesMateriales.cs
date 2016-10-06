c51ffbda96fdea77c4966196af5e224187eb0093
ihugojimenez <hugofernando809@hotmail.com>
2016-10-06 11:44:25 -0400

    Iniciando el diseño de la ventana, creando la clase para el detalle (SolicitudesMateriales)

---
+++ ParcialWebApplication/BLL/SolicitudesMateriales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    public class SolicitudesMateriales
+    {
+        public int Id { get; set; }
+        public int IdSolicitud { get; set; }
+        public int idMaterial { get; set; }
+        public int Cantidad { get; set; }
+
+        public SolicitudesMateriales(int Idsolicitud, int IdMaterial, int Cantidad)
+        {
+            this.IdSolicitud = Idsolicitud;
+            this.idMaterial = IdMaterial;
+            this.Cantidad = Cantidad;
+        }
+    }
+}
